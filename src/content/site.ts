96caf8afc4a947ec35d4eb8761bad48ab079d0a3
rombenson <rombensonwork@gmail.com>
2026-04-28 13:19:44 +0300

    Update route links to Yandex Maps

--- src/content/site.ts
+++ src/content/site.ts
@@ -9,8 +9,7 @@ export const site = {
   reviewsCount: "25 отзывов",
   ratingsCount: "33 оценки",
   award: "Хорошее место 2026",
-  routeUrl:
-    "https://yandex.ru/maps/?text=%D0%91%D0%B0%D0%BB%D0%B0%D1%88%D0%B8%D1%85%D0%B0%2C%20%D0%BC%D0%BA%D1%80.%20%D0%96%D0%B5%D0%BB%D0%B5%D0%B7%D0%BD%D0%BE%D0%B4%D0%BE%D1%80%D0%BE%D0%B6%D0%BD%D1%8B%D0%B9%2C%20%D0%A2%D0%B5%D0%BC%D0%BD%D0%B8%D0%BA%D0%BE%D0%B2%D0%BE%2C%2093",
+  routeUrl: "https://yandex.com/maps/-/CPSEnPJf",
   mapUrl:
     "https://yandex.ru/map-widget/v1/?text=%D0%91%D0%B0%D0%BB%D0%B0%D1%88%D0%B8%D1%85%D0%B0%2C%20%D0%BC%D0%BA%D1%80.%20%D0%96%D0%B5%D0%BB%D0%B5%D0%B7%D0%BD%D0%BE%D0%B4%D0%BE%D1%80%D0%BE%D0%B6%D0%BD%D1%8B%D0%B9%2C%20%D0%A2%D0%B5%D0%BC%D0%BD%D0%B8%D0%BA%D0%BE%D0%B2%D0%BE%2C%2093&z=15",
   payments: ["карта", "наличные", "банковский перевод"],
